70c6440260175d2f77337d2355522a71512fa5d8
Piyush Bhoye <piyushbhoye05@gmail.com>
2026-03-17 16:41:03 +0000

    fixed port binding for render

--- app.py
+++ app.py
@@ -213,5 +213,8 @@ def logout():
     return redirect('/login')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
